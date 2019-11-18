24ca3eb9b77bde64abd0f8bf709be34d34507d05
Juan-Pablo Scaletti <juanpablo@jpscaletti.com>
2019-11-17 22:26:37 -0500

    set CSRF token in the response headers

--- proper/plugs/protect_from_forgery.py
+++ proper/plugs/protect_from_forgery.py
@@ -20,15 +20,16 @@ __all__ = (
 
 CSRF_SESSION_KEY = "__csrf_token"
 CSRF_QUERY_KEY = "_csrf_token"
-CSRF_HEADER = "X-CSRFToken"
-CSRF_HEADER_ALT = "X-CSRF-TOKEN"
+CSRF_HEADER = "X-CSRF-TOKEN"
+CSRF_HEADER_ALT = "X-CSRFToken"
 
 
 def protect_from_forgery(req, resp, app):
-    if resp.dispatched:
-        return
+    req_token = get_or_set_token(req)
 
-    get_or_set_token(req)
+    if resp.dispatched:
+        resp.headers[CSRF_HEADER] = req_token
+        return
 
     if not req.must_check_csrf():
         return
@@ -38,18 +39,14 @@ def protect_from_forgery(req, resp, app):
         raise MissingCSRFToken(
             "Missing Cross-Site Request Forgery (CSRF) token. "
             f"You must provide the token value as a “{CSRF_QUERY_KEY}” form field, "
-            f"a query field with the same name, or in a “{CSRF_HEADER}” header."
+            f"a query field with the same name, in a “{CSRF_HEADER}” header."
         )
 
-    if not request_token_is_valid(req, token):
+    if req_token != token:
         raise InvalidCSRFToken(
             "Invalid Cross-Site Request Forgery (CSRF) token. "
             "The token provided doesn't match the one stored in the session."
         )
-
-
-def request_token_is_valid(req, token):
-    return get_or_set_token(req) == token
 
 
 def get_used_token(req):
