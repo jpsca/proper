63db769c34850f4f7a2b511961651b5435557a8c
Juan-Pablo Scaletti <juanpablo@jpscaletti.com>
2020-08-18 13:59:59 -0500

    REST-RPC

--- proper/router/resources.py
+++ proper/router/resources.py
@@ -4,22 +4,32 @@ from .route import Route
 __all__ = ("resources", )
 
 
-REST_ROUTES = (
+ROUTES = (
     ("GET", "/", "index"),
-    ("GET", "/:uid", "show"),
     ("GET", "/new", "new"),
-    ("POST", "/", "create"),
+    ("POST", "/create", "create"),
+    ("GET", "/:uid", "show"),
     ("GET", "/:uid/edit", "edit"),
-    ("PATCH", "/:uid", "update"),
-    ("PUT", "/:uid", "update"),
-    ("DELETE", "/:uid", "delete"),
+    ("POST", "/:uid/update", "update"),
+    ("POST", "/:uid/delete", "delete"),
 )
 
-REST_ACTIONS = ("index", "show", "new", "create", "edit", "update", "delete")
+ACTIONS = ("index", "new", "create", "show", "edit", "update", "delete")
 
 
-def resources(path, to, only=REST_ACTIONS, ignore=None, **kwargs):
-    """Shortcut to return a list of HTTP REST routes with the same arguments.
+def resources(path, to, only=ACTIONS, ignore=None, **kwargs):
+    """Shortcut to return a list of REST-RPC routes with the same arguments:
+
+    METHOD | PATH           | ENDPOINT
+    ----------------------------------------
+    GET       /               index
+    GET       /new            new
+    POST      /create         create
+    GET       /:uid           show
+    GET       /:uid/edit      edit
+    POST      /:uid/update    update
+    POST      /:uid/delete    delete
+    ----------------------------------------
 
     We calidate the arguments first so we can show errors about what the user has
     typed instead of being about dynamically generated routes.
@@ -28,7 +38,7 @@ def resources(path, to, only=REST_ACTIONS, ignore=None, **kwargs):
 
     ignore = ignore or []
     _actions = [
-        action for action in only if (action in REST_ACTIONS) and (action not in ignore)
+        action for action in only if (action in ACTIONS) and (action not in ignore)
     ]
     assert _actions, "None of the actions are valid."
     return expand_resources(res, _actions)
@@ -36,7 +46,7 @@ def resources(path, to, only=REST_ACTIONS, ignore=None, **kwargs):
 
 def expand_resources(res, actions):
     routes = []
-    for method, path, action in REST_ROUTES:
+    for method, path, action in ROUTES:
         if action not in actions:
             continue
         route = expand_resources_route(res, method, path, action)
