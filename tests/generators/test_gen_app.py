138b41edd4d7a09e8a2ab3c69c72b001622e90d2
Juan-Pablo Scaletti <juanpablo@jpscaletti.com>
2024-11-14 16:15:46 -0500

    fix gen app tests

--- tests/generators/test_gen_app.py
+++ tests/generators/test_gen_app.py
@@ -2,32 +2,30 @@ from proper.generators import gen_app
 
 
 def test_gen_app(tmp_path):
-    name = "app"
+    name = "myapp"
     root = tmp_path / name
     gen_app(root, force=True, _is_a_test=True)
-    assert (root / name).is_dir()
     assert (root / "static").is_dir()
-    assert (root / name / "config").is_dir()
-    assert (root / name / "controllers").is_dir()
-    assert (root / name / "forms").is_dir()
-    assert (root / name / "models").is_dir()
-    assert (root / name / "views").is_dir()
+    assert (root / "config").is_dir()
+    assert (root / "app" / "controllers").is_dir()
+    assert (root / "app" / "forms").is_dir()
+    assert (root / "app" / "models").is_dir()
+    assert (root / "app" / "views").is_dir()
 
-    assert (root / name / "app.py").exists()
-    assert (root / name / "router.py").exists()
+    assert (root / "app" / "main.py").exists()
+    assert (root / "app" / "router.py").exists()
 
 
 def test_gen_app_custom(tmp_path):
-    name = "app"
+    name = "myapp"
     root = tmp_path / "project"
     gen_app(root, name=name, force=True, _is_a_test=True)
-    assert (root / name).is_dir()
     assert (root / "static").is_dir()
-    assert (root / name / "config").is_dir()
-    assert (root / name / "controllers").is_dir()
-    assert (root / name / "forms").is_dir()
-    assert (root / name / "models").is_dir()
-    assert (root / name / "views").is_dir()
+    assert (root / "config").is_dir()
+    assert (root / "app" / "controllers").is_dir()
+    assert (root / "app" / "forms").is_dir()
+    assert (root / "app" / "models").is_dir()
+    assert (root / "app" / "views").is_dir()
 
-    assert (root / name / "app.py").exists()
-    assert (root / name / "router.py").exists()
+    assert (root / "app" / "main.py").exists()
+    assert (root / "app" / "router.py").exists()
