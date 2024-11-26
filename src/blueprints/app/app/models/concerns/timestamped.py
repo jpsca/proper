427d4bc0629b39cacb6f8ab21229909214bf6e84
Juan-Pablo Scaletti <juanpablo@jpscaletti.com>
2024-11-26 17:50:52 -0500

    timestamped

--- src/blueprints/app/app/models/concerns/timestamped.py
+++ src/blueprints/app/app/models/concerns/timestamped.py
@@ -1,14 +1,17 @@
-from datetime import datetime
-
 import peewee as pw
 
 from app.models.base import BaseMixin
 
 
 class Timestamped(BaseMixin):
-    created_at = pw.DateTimeField(default=datetime.utcnow, null=True)
-    updated_at = pw.DateTimeField(default=datetime.utcnow, null=True)
+    created_at = pw.DateTimeField(default=pw.utcnow, null=True)
+    updated_at = pw.DateTimeField(default=pw.utcnow, null=True)
 
-    def update(self, *args, **kwargs):
-        self.updated_at = datetime.utcnow()
+    @classmethod
+    def update(cls, *args, **kwargs):
+        kwargs["updated_at"] = pw.utcnow()
         return super().update(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        self.updated_at = pw.utcnow()
+        return super().save(*args, **kwargs)
