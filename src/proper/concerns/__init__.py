ef2366376a71d87f310a787874d192004168cfcf
Juan-Pablo Scaletti <juanpablo@jpscaletti.com>
2026-02-25 13:52:42 -0500

    Fix typo

--- src/proper/concerns/__init__.py
+++ src/proper/concerns/__init__.py
@@ -1,5 +1,5 @@
 from .current_locale import *  # noqa
 from .current_timezone import *  # noqa
-from .orign_protection import *  # noqa
+from .origin_protection import *  # noqa
 from .rate_limiting import *  # noqa
 from .request_forgery_protection import *  # noqa
